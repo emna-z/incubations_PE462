c3c2ed6a681217fd38412e8a400d10373ecec750
Emna Zeghal <emna.zeghal@nioz.nl>
2021-02-25 20:53:25 +0100

    initial files

---
+++ asv_NIOZ186_analysis/library_NIOZ186_asv_pipeline_results_analysis.R
@@ -0,0 +1,14 @@
+##############################Scrpit_to_study_the_asv_pipeline_results_for_library_NIOZ186#############################
+#install.packages("tidyverse")
+library(tidyverse)
+#install.packages("vegan")
+library("vegan")
+#if (!requireNamespace("BiocManager", quietly = TRUE))
+#  install.packages("BiocManager")
+#BiocManager::install("phyloseq")
+library("phyloseq")
+# install.packages("FactoMineR")
+library("FactoMineR")
+#install.packages("factoextra")
+library("factoextra")
+#####################Import_data##################
